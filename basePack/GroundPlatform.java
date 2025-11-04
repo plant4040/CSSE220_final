a5c22baec9396ba7cb898cc3dc8e744403af0e44
burksdc <burksdc@rose-hulman.edu>
2025-11-04 13:54:38 -0500

    Made first sprite - used for the ground platforms

--- basePack/GroundPlatform.java
+++ basePack/GroundPlatform.java
@@ -1,22 +1,71 @@
 package basePack;
 
 import java.awt.Graphics;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+
 import java.awt.Color;
 
 public class GroundPlatform {
- private int x, y, width, height;
-
+ 
+	private int x, y, width, height;
+	Color color = Color.RED;
+	private BufferedImage sprite;
+ 
+ /*
+ * 
+ * 
+ * 
+ */
+	boolean spriteLoaded = false;
+ 
  public GroundPlatform(int x, int y, int width, int height) {
      this.x = x;
      this.y = y;
      this.width = width;
      this.height = height;
      
+     
+     
+     try {
+         sprite = ImageIO.read(GroundPlatform.class.getResource("Grass Block-1.png.png"));
+         spriteLoaded = true;
+         } catch (IOException e) {
+         	spriteLoaded = false;
+         }
  }
-
+ 
+ 
+ 
+ 
  public void draw(Graphics g) {
-     g.setColor(Color.DARK_GRAY);
-     g.fillRect(x, y, width, height);
- }
-}
+ 	
+ 	int drawX = x;
+ 	int drawY = y;
+ 	int drawHeight = height;
+ 	int drawWidth = width;
+ 	
+ 	if (spriteLoaded) {
+ 		g.drawImage(sprite, drawX, drawY, drawWidth,drawHeight, null );
+ 	} else {
+ 		  g.setColor(color);
 
+ 	}
+   
+ }
+ 
+ 
+ 
+ 
+// Current stand in for Color 
+// public void draw(Graphics g) {
+//     g.setColor(Color.RED);
+//     g.fillRect(x, y, width, height);
+// }
+//}
+
+}
