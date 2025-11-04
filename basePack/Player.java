2fc286cc5c9a7ff43745c5dd6a794704c4531152
Evan McElwain <evanmcelwain2025@gmail.com>
2025-11-04 13:12:53 -0500

    Player movement methods

--- basePack/Player.java
+++ basePack/Player.java
@@ -6,6 +6,14 @@ package basePack;
  */
 public class Player extends Entity {
 	
+	//all subject to change
+	private static final int XACCELERATION = 5;
+	private static final int GACCELERATION = 10;
+	private static final int JUMPACCELERATION = 50;
+	private static final int MAXHORIZONTALVELO = 20;
+	private static final int MAXVERTICALVELO = 100;
+	private boolean onGround;
+	
 	public Player(int xPos,int yPos) {
 		this.xPos = xPos;
 		this.yPos = yPos;
@@ -13,5 +21,32 @@ public class Player extends Entity {
 		this.yVelo = 0;
 	}
 	
+	public void MoveLeft() {
+		if (xVelo - XACCELERATION < -MAXHORIZONTALVELO) {
+			xVelo = -MAXHORIZONTALVELO;
+		}
+		else {
+			xVelo -= XACCELERATION;
+		}
+	}
+	
+	public void MoveRight() {
+		if (xVelo+XACCELERATION > MAXHORIZONTALVELO) {
+			xVelo = MAXHORIZONTALVELO;
+		}
+		else{
+			xVelo += XACCELERATION;
+		}
+	}
+	
+	public void Jump() {
+		yVelo += JUMPACCELERATION;
+	}
+	
+	public void update() {
+		xPos += xVelo;
+		//may need to add in friction decrease in xVelo, may be different based on whether in the air or on the ground
+		
+	}
 	
 }
